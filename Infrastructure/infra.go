53a0c4493c82c62489006afbd95aa1444d558103
Surafel Belachew <148704749+surafelbkassa@users.noreply.github.com>
2025-09-13 10:27:10 +0300

    start register endpoint

---
+++ Infrastructure/infra.go
@@ -0,0 +1,27 @@
+package infrastructure
+
+import (
+    "sync"
+    "github.com/surafelbkassa/go-distributed-job-queue/Domain"
+    "github.com/surafelbkassa/go-distributed-job-queue/Repository"
+)
+
+type InMemoryUserRepository struct {
+    users []*domain.User
+    mu    sync.Mutex
+}
+
+func NewInMemoryUserRepository() *InMemoryUserRepository {
+    return &InMemoryUserRepository{
+        users: make([]*domain.User, 0),
+    }
+}
+
+func (r *InMemoryUserRepository) Save(user *domain.User) error {
+    r.mu.Lock()
+    defer r.mu.Unlock()
+    r.users = append(r.users, user)
+    return nil
+}
+
+// Optionally, add methods like FindByEmail, etc.
